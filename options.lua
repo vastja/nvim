3c46d95fc444d3f19b9c48c482bef49c5e38859b
vastja <vastja@gmail.com>
2024-07-06 11:00:48 +0200

    Tab, ui options

---
+++ options.lua
@@ -0,0 +1,12 @@
+vim.opt.clipboard = 'unnamedplus' -- use system clipboard
+
+-- UI config
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+-- Tab
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
