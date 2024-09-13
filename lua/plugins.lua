104b1a1b8e095f5b7954a4b0491293f01f2d8666
Jakub Vasta <j.vasta@tricentis.com>
2024-09-13 14:33:35 +0200

    dap ui

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -172,4 +172,14 @@ require("lazy").setup({
 			"mfussenegger/nvim-dap",
 		},
 	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"nvim-neotest/nvim-nio",
+		},
+		config = function()
+			require("dapui").setup()
+		end,
+	},
 })
